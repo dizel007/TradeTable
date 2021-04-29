7fd8008c319e7e1e96af944162c0462cdff6ae82
Dmitriy <dizel007@yandex.ru>
2021-04-29 17:35:57 +0500

    Улучшил отображение таблицы ЭКСЕЛЬ

--- open_excel/simplexlsx.php
+++ open_excel/simplexlsx.php
@@ -13,20 +13,26 @@ $file_xlsx_name ="../". $_GET['LinkKp'];
 
 		list( $cols, ) = $xlsx->dimension();
 $str=0;
+$stop=0;  
 		foreach ( $xlsx->rows() as $k => $r ) {
-					if ($k == 0) continue; // skip first row
-				if ($str<13) {
-					$str++;
-					continue;
-				}
+			 if ($k == 0) continue; // skip first row
+			 if ($str<13) { $str++; continue;}
+			 if (empty($r[2])) {$stop++;} // находим конец таблицы с ценам и обрезаем после полной цены
 
 			echo '<tr>';
-			for ( $i = 3; $i < $cols; $i ++ ) {
-				if (($i==4) or ($i==5) or ($i==6) or ($i==7) or ($i==9) or ($i > 12)) {continue;}
-				echo '<td>' . ( ( isset( $r[ $i ] ) ) ? $r[ $i ] : '&nbsp;' ) . '</td>';
+			for ( $i = 2; $i < $cols; $i ++ ) {
+				if (($i==4) or ($i==5) or ($i==6) or ($i==7) or ($i==9) or ($i > 12)) {continue;} // Убираем лишние столбцы
+											
+				if ($stop == 1) {$r[11] = "ИТОГО :";}  
+				if ($stop == 2) {$r[11] = "НДС20% :";}  
+				
+					echo '<td>' . ( ( isset( $r[ $i ] ) ) ? $r[ $i ] : '&nbsp;' ) . '</td>';
 			}
 			echo '</tr>';
-			$str++;
+			
+		  if ($stop==2) {break;}
+
+			$str++; // переменная чтобы найти строку начала таблциы
 		}
 		echo '</table>';
 	} else {
