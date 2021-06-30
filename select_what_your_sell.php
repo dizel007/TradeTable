5021f8dca3b98349ee41950b3b0a834b3f8840ca
Dmitriy <dizel007@yandex.ru>
2021-06-30 15:42:24 +0500

    Добавил сумму проданных КП

--- select_what_your_sell.php
+++ select_what_your_sell.php
@@ -12,7 +12,20 @@ require_once ("functions/print_our_table.php");
   $arr_name = selectArrByKpCondition($mysqli, $KpCondition);
     
   echo "КП которые купили у нас :";
-  
+ $summa =0;
+
+// Высчитываем сумму проданных КП
+  foreach ($arr_name as $key => $value) {
+    foreach ($value as $key1 => $value1) {
+      if ($key1 == 'KpSum') {
+        $summa = $summa + $value1;
+      }
+    }
+  }
+
+
+  echo "СУММА  =". number_format($summa)."<br>";
+
  printOurTable($arr_name, $FinishContract);
 //////// сюда же добавим компании с этим же инн
 
