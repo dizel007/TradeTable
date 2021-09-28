03b5ea5cefdbebb5c30701af90852cbe7dc913ca
Dmitriy <dizel007@yandex.ru>
2021-09-28 18:08:24 +0500

    Добавил логирование
    Теперь есть логи по людям

--- changeDB/update_all_zakup.php
+++ changeDB/update_all_zakup.php
@@ -86,12 +86,13 @@ printf("Соединение не удалось: ");
 
 
 
-//       $fileLogName = date('Y-m-d'); // создаем имя фаила куда будем писать логи ... каждый день новый файил
-      
-//       $file = "../logs/".$fileLogName.".txt";
+$fileLogName = date('Y-m-d'); // создаем имя фаила куда будем писать логи ... каждый день новый файил
+$file = "../logs/".$fileLogName.".txt";
+$fileUser = "../logs/".$user_login.".txt";
        $fileAll = '../log.txt';
        $now_date = date('Y-m-d H:i:s');
        $temp_var = $now_date." ID=".$id;
+       $temp_var .= ": Автор: ".$user_login;
        $temp_var.="; Важность :".$KpImportance;
        $temp_var.="; Ответственный :".$Responsible;
        $temp_var.="; Комментарий :".$Comment;
@@ -101,45 +102,15 @@ printf("Соединение не удалось: ");
        $temp_var.="; Адрес :".$Adress;
        $temp_var.=";\n";
 
-//  // Форсурием переменную для записи в ЛОГфайл
-
-// $temp_var = $now_date." Автор: ".$user_login." ID=".$id;
-//       if ($my_id_arr[0]['ContactCustomer']== $ContactCustomer) { $ContactCustomer='';} 
-//             else { $temp_var.="; Контакты Заказчика :".$ContactCustomer;}
-      
-//       if ($my_id_arr[0]['KpImportance']== $KpImportance) { $KpImportance='';}
-//             else { $temp_var.="; Важность :".$KpImportance;}
-
-//       if ($my_id_arr[0]['Responsible']== $Responsible) { $Responsible='';}
-//             else { $temp_var.="; Ответственный :".$Responsible;}
-      
-//       if ($my_id_arr[0]['Comment']== $Comment) { $Comment='';}
-//             else { $temp_var.="; Комментарий :".$Comment;}
-
-//       if ($my_id_arr[0]['KpSum']== $KpSum) { $KpSum='';}
-//             else { $temp_var.="; Сумма КП = :".$KpSum;}
-                        
-//       if ((string)$my_id_arr[0]['DateNextCall'] == (string)$DateNextCall || (string)$my_id_arr[0]['DateNextCall'] == '') { $DateNextCall = '';}
-//             else { $temp_var.="; Дата след.Звонка :".$DateNextCall;}
-      
-//       if ($my_id_arr[0]['KpCondition']== $KpCondition) { $KpCondition='';}
-//             else { $temp_var.="; Состояние КП :".$KpCondition;}
-      
-//       if ($my_id_arr[0]['FinishContract']== $FinishContract) { $FinishContract='';}
-//             else { $temp_var.="; Контракт Закрыт :".$FinishContract;}
-
-//       if ($my_id_arr[0]['Adress']== $Adress) { $Adress='';}
-//             else { $temp_var.="; Адрес :".$Adress;}
-//       $temp_var.=";\n";
-      
+//  Форсурием переменную для записи в ЛОГфайл
 //       // Пишем содержимое в файл,
 //       // используя флаг FILE_APPEND для дописывания содержимого в конец файла
 //       // и флаг LOCK_EX для предотвращения записи данного файла кем-нибудь другим в данное время
 
-//       file_put_contents($file, $temp_var, FILE_APPEND | LOCK_EX); // логи по датам
-
+ 
+       file_put_contents($file, $temp_var, FILE_APPEND | LOCK_EX); // логи по датам
        file_put_contents($fileAll, $temp_var, FILE_APPEND | LOCK_EX); // Все логи подряд
-
+       file_put_contents($fileUser, $temp_var, FILE_APPEND | LOCK_EX); // Все логи подряд
 
 // header ("Location: ..?id=".$id);  // перенаправление на нужную страницу
 // exit();    // прерываем работу скрипта, чтобы забыл о прошлом
